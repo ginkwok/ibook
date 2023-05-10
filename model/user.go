d435d9f19cd95ffb3435903ad3d45dbcaa892e1a
Gin Kwok <123966106+ginkwok@users.noreply.github.com>
2023-05-10 17:07:31 +0800

    Update user.go fixbug username unique index without size bug

--- model/user.go
+++ model/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `json:"username" gorm:"uniqueIndex"`
-	Password string `json:"password"`
+	Username string `json:"username" gorm:"uniqueIndex;size:255;not null"`
+	Password string `json:"password" gorm:"not null"`
 	Email    string `json:"email"`
 }
 
