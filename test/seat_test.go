087333a6ef5e0ba1dbebe6f6a959cb6a4b9ee805
gin <ginkwok@outlook.com>
2023-06-08 01:07:18 +0800

    update seat test func

--- test/seat_test.go
+++ test/seat_test.go
@@ -16,16 +16,34 @@ func TestGetSeat(t *testing.T) {
 
 func TestCreateSeat(t *testing.T) {
 
+	t.Run("Test create seat", func(t *testing.T) {
+
+	})
+
+	t.Run("Test create existed seat", func(t *testing.T) {
+	})
+
 }
 
 func TestDeleteSeat(t *testing.T) {
+	t.Run("Test delete seat", func(t *testing.T) {
+	})
+	t.Run("Test delete seat that don't exist", func(t *testing.T) {
+	})
 
 }
 
 func TestUpdateSeat(t *testing.T) {
+	t.Run("Test update seat", func(t *testing.T) {
+	})
+	t.Run("Test update seat that don't exist", func(t *testing.T) {
+	})
 
 }
 
 func TestSearchSeat(t *testing.T) {
-
+	t.Run("Test search seat", func(t *testing.T) {
+	})
+	t.Run("Test search seat with empty query", func(t *testing.T) {
+	})
 }
